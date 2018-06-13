63df35055860817bbd29e2f29219a657072bab5e
Rachmat Hidayat <37132165+rachmathidayatdev@users.noreply.github.com>
2018-06-13 11:34:08 +0700

    Create JobsSendQueueEmail.php

---
+++ app/Jobs/JobsSendQueueEmail.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Mail\TestingMail;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Mail\Mailer;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Support\Facades\Mail;
+
+class JobsSendQueueEmail implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+
+    protected $email;
+
+    public function __construct($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        //send email
+        Mail::to($this->email)->send(new TestingMail());
+        //sampai sini
+    }
+}
